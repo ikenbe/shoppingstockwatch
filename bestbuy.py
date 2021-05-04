9631f9f3ee4dcf53635b58ee7d7dd32ee495eef8
ikenbe <ikenbe@gmail.com>
2021-05-04 15:43:13 -0700

    update

--- bestbuy.py
+++ bestbuy.py
@@ -1,13 +1,21 @@
 import urllib3
 from lxml import etree
 
+
 def check(prdct_link):
-    user_agent = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75'}
-    http = urllib3.PoolManager(10,headers=user_agent)
-    r = http.request('GET',prdct_link)
-    tree = etree.HTML(r.data)
-    avail = tree.xpath('//div[contains(@class,"availability")]//text()')
-    for text in avail:
-        if (text.find('Coming soon') + text.find('Sold out') + text.lower().find('unavailable') + text.find('Check other stores') == -4):
+    user_agent = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75"
+    }
+    http = urllib3.PoolManager(10, headers=user_agent)
+    r = http.request("GET", prdct_link)
+    tree = etree.HTML(r.data).xpath('//div[contains(@class,"x-pdp-availability-online")]//text()')
+    # avail = tree.xpath('//div[contains(@class,"availability")]//text()')
+    # for text in avail:
+    #    if (text.find('Coming soon') + text.find('Sold out') + text.lower().find('unavailable') + text.find('Check other stores') == -4):
+    #        return True
+    for text in tree:
+        if text.find("Available to ship") > -1:
+            #count += 1
+            #if count > 1:
             return True
     return False
--- gpu.py
+++ gpu.py
@@ -4,37 +4,104 @@ import random
 import bestbuy
 import amazon
 
+
 class Product:
-    def __init__(self,name,bestbuyLink,amazonLink):
+    def __init__(self, name, bestbuyLink, amazonLink):
         self.name = name
         self.bestbuyLink = bestbuyLink
         self.amazonLink = amazonLink
+
     instock = False
+
     def checkStock(self):
-        if (self.bestbuyLink):
+        if self.bestbuyLink:
             bestbuy.check(self.bestbuyLink)
+
+
 ##########
-PS5Full = Product('PS5 Disc','https://www.bestbuy.ca/en-ca/product/playstation-5-console-online-only/14962185','https://www.amazon.ca/PlayStation-5-Console/dp/B08GSC5D9G/')
-PS5Digital = Product('PS5 Digital','https://www.bestbuy.ca/en-ca/product/playstation-5-digital-edition-console-online-only/14962184','https://www.amazon.ca/Playstation-3005721-PlayStation-Digital-Edition/dp/B08GS1N24H')
+nv3060Ti = Product(
+    "Nvidia 3060 Ti",
+    "https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3060-ti-8gb-gddr6-video-card/15166285",
+    "",
+)
+nv3070 = Product(
+    "Nvidia 3070",
+    "https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3070-8gb-gddr6-video-card-only-at-best-buy/15078017",
+    "",
+)
+evga3060Ti = Product(
+    "evga3060ti",
+    "https://www.bestbuy.ca/en-ca/product/evga-nvidia-geforce-rtx-3060-ti-ftw3-ultra-8gb-gddr6-video-card/15229237",
+    "",
+)
+evga3070 = Product(
+    "evga3070",
+    "https://www.bestbuy.ca/en-ca/product/evga-geforce-rtx-3070-xc3-ultra-8gb-gddr6-video-card/15147122",
+    "",
+)
+evga3080 = Product(
+    "evga3080",
+    "https://www.bestbuy.ca/en-ca/product/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6x-video-card/15084753",
+    "",
+)
+msi3060ti = Product(
+    "MSI 3060 Ti",
+    "https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3060-ti-ventus-2x-oc-8gb-gddr6-video-card/15178453",
+    "",
+)
+msi3070 = Product(
+    "MSI 3070",
+    "https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3070-ventus-3x-oc-8gb-gddr6-video-card/15038016",
+    "",
+)
+msi3080 = Product(
+    "MSI 3080",
+    "https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3080-ventus-3x-10gb-gddr6x-video-card/14950588",
+    "",
+)
+asustuf3080 = Product(
+    "ASUS TUF 3080",
+    "https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3080-ventus-3x-10gb-gddr6x-video-card/14950588",
+    "",
+)
+asusrog3080 = Product(
+    "ASUS ROG 3080",
+    "https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3080-ventus-3x-10gb-gddr6x-video-card/14950588",
+    "",
+)
+testobj = Product(
+    "Corsair TM30 Performance Thermal Paste",
+    "https://www.bestbuy.ca/en-ca/product/corsair-tm30-performance-thermal-paste/14193869",
+    "",
+)
 ##########
-bbProducts = [PS5Full,PS5Digital]
-amznProducts = [PS5Full,PS5Digital]
+bbProducts = [
+    nv3060Ti,
+    nv3070,
+    evga3060Ti,
+    evga3070,
+    evga3080,
+    msi3060ti,
+    msi3070,
+    msi3080,
+    asusrog3080,
+    asustuf3080,
+]
+amznProducts = []
 ##########
 def checkAll():
     for p in bbProducts:
-        if (p.bestbuyLink != ''):
-            p.instock = bestbuy.check(p.bestbuyLink)
-        if (p.instock == True):
-            telegram_send.send(messages=[p.name,'seems be be in stock'])
-            print(time.ctime(), 'Alert!!', p.name, 'in stock BestBuy!')
+        if p.bestbuyLink != "" and bestbuy.check(p.bestbuyLink) == True:
+            telegram_send.send(messages=[p.name, "seems be be in stock BestBuy"])
+            print(time.ctime(), "Alert!!", p.name, "in stock BestBuy!")
     for q in amznProducts:
-        if (q.amazonLink != ''):
-            q.instock = amazon.check(q.amazonLink)
-        if (q.instock == True):
-            telegram_send.send(messages=[q.name,'seems be be in stock'])
-            print(time.ctime(), 'Alert!!', q.name, 'in stock Amazon!')
+        if q.amazonLink != "" and amazon.check(q.amazonLink) == True:
+            telegram_send.send(messages=[q.name, "seems be be in stock Amazon"])
+            print(time.ctime(), "Alert!!", q.name, "in stock Amazon!")
 
+
+print(time.ctime(), "Script Start")
 while True:
     checkAll()
-    print(time.ctime(), 'Out of Stock')
-    time.sleep( 12.5 - random.random()*5)
+    print(time.ctime(), "No Stock Found. Staring New Cycle")
+    time.sleep(12.5 - random.random() * 5)
